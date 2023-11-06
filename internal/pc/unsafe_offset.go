e4c484a012c96138339a7f3364e0d8659b9b48f4
Prashant V <prashant@prashantv.com>
2023-11-05 19:29:13 -0800

    Fix pointer arithmetic caught by `-race`
    When doing pointer arithmetic, the conversion from `unsafe.Pointer` to
    `uintptr`, and then back, must be within a single expression.
    
    From https://pkg.go.dev/unsafe#Pointer
    > // INVALID: uintptr cannot be stored in variable
    > // before conversion back to Pointer.
    
    This is caught by `-race`, so enable it in the Makefile.
    
    Note that `-race` is only supported for amd64/arm64, so disable
    it on other architectures using the GitHub action.

--- internal/pc/unsafe_offset.go
+++ internal/pc/unsafe_offset.go
@@ -5,7 +5,7 @@ package pc
 import "unsafe"
 
 func derefAddr(addr unsafe.Pointer, subOffset uintptr) uintptr {
-	addrOffset := (uintptr)(addr) - subOffset
-	addrPtr := (*uintptr)(unsafe.Pointer(addrOffset))
+	addrOffset := unsafe.Pointer((uintptr)(addr) - subOffset)
+	addrPtr := (*uintptr)(addrOffset)
 	return *addrPtr
 }
