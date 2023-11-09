1527ec7b1d35114332e4a67aee509666438e35f4
Prashant Varanasi <prashant@prashantv.com>
2023-11-09 07:50:04 -0800

    Use `sync.Pool` to allocate arena slabs and reduce contention (#9)
    Use `sync.Pool` to find slabs which should reduce contention as
    `sync.Pool` uses per-P local pools, so the `arenaSlab` should have less
    contention.
    
    Benchmarks on amd64:
    ```
            │  no-pool.txt  │                pool.txt                │
            │    sec/op     │    sec/op      vs base                 │
    Wrap       9.455n ± ∞ ¹   13.930n ± ∞ ¹        ~ (p=0.100 n=3) ²
    Wrap-2    31.390n ± ∞ ¹    7.861n ± ∞ ¹        ~ (p=0.100 n=3) ²
    Wrap-4    31.850n ± ∞ ¹    5.131n ± ∞ ¹        ~ (p=0.100 n=3) ²
    Wrap-8    32.990n ± ∞ ¹    5.363n ± ∞ ¹        ~ (p=0.100 n=3) ²
    geomean    23.63n          7.409n        -68.65%
    ```
    
    The slab alloc is faster when there's no contention, but significantly
    slower when there is. Using the pool avoids this contention, but means
    the no contention case is a little slower than using the arena, though
    still faster than no arena.

--- arena.go
+++ arena.go
@@ -2,44 +2,33 @@ package errtrace
 
 import (
 	"sync"
-	"sync/atomic"
 )
 
 // arena is a lock-free allocator for a fixed-size type.
 // It is intended to be used for allocating errTrace objects in batches.
 type arena[T any] struct {
-	// slab is the current slab of objects.
-	//
-	// When this runs out, a new slab will be swapped in.
-	slab atomic.Pointer[arenaSlab[T]]
+	slabSize int
+	pool     sync.Pool
 }
 
-// newArena returns a new arena with the given slab size.
-func newArena[T any](sz int) *arena[T] {
-	var a arena[T]
-	a.slab.Store(newArenaSlab[T](sz))
-	return &a
+func newArena[T any](slabSize int) *arena[T] {
+	return &arena[T]{
+		slabSize: slabSize,
+	}
 }
 
 // Take returns a pointer to a new object from the arena.
 func (a *arena[T]) Take() *T {
 	for {
-		slab := a.slab.Load()
-		if e, ok := slab.take(); ok {
-			return e
+		slab, ok := a.pool.Get().(*arenaSlab[T])
+		if !ok {
+			slab = newArenaSlab[T](a.slabSize)
 		}
 
-		// Slab is exhausted, replace it.
-		// The sync.Once ensures that
-		// the first goroutine to get here does the replacement,
-		// and all others either wait for it to finish
-		// or arrive after the replacement is done.
-		//
-		// Everyone tries with the new slab in the next iteration.
-		slab.replace.Do(func() {
-			newSlab := newArenaSlab[T](len(slab.buf))
-			a.slab.CompareAndSwap(slab, newSlab)
-		})
+		if e, ok := slab.take(); ok {
+			a.pool.Put(slab)
+			return e
+		}
 	}
 }
 
@@ -52,10 +41,7 @@ type arenaSlab[T any] struct {
 	buf []T
 
 	// Index of the next object to be taken.
-	idx atomic.Int64
-
-	// Ensures that the slab is replaced only once.
-	replace sync.Once
+	idx int
 }
 
 func newArenaSlab[T any](sz int) *arenaSlab[T] {
@@ -63,9 +49,10 @@ func newArenaSlab[T any](sz int) *arenaSlab[T] {
 }
 
 func (a *arenaSlab[T]) take() (*T, bool) {
-	idx := a.idx.Add(1) - 1 // 0-indexed
-	if int(idx) >= len(a.buf) {
+	if a.idx >= len(a.buf) {
 		return nil, false
 	}
-	return &a.buf[idx], true
+	ptr := &a.buf[a.idx]
+	a.idx++
+	return ptr, true
 }
