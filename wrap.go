13f628494664dbb80cbe1bb9d8b686c3d907f957
Prashant V <prashant@prashantv.com>
2023-11-20 18:36:14 -0800

    Remove debugging leftovers

--- wrap.go
+++ wrap.go
@@ -1,9 +1,6 @@
 package errtrace
 
 import (
-	"fmt"
-	"runtime"
-
 	"braces.dev/errtrace/internal/pc"
 )
 
@@ -18,26 +15,6 @@ func Wrap(err error) error {
 	}
 
 	return wrap(err, pc.GetCaller())
-}
-
-func GetCallerSafe() uintptr {
-	const skip = 1 + // frame for Callers
-		1 + // frame for GetCaller
-		1 // frame for our caller, which should be errtrace.Wrap
-
-	var callers [1]uintptr
-	n := runtime.Callers(3, callers[:]) // skip getcallerpc + caller
-	if n == 0 {
-		return 0
-	}
-	return callers[0]
-}
-
-func printFrame(pc uintptr) {
-	frames := runtime.CallersFrames([]uintptr{pc})
-	f, _ := frames.Next()
-	fmt.Printf("  FILELINE  %v\n    %v:%v\n", f.Function, f.File, f.Line)
-
 }
 
 // Wrap2 is used to `Wrap` the last error return when returning 2 values.
