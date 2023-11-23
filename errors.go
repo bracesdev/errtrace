1407ab46eb90ef952caceba8918f2baa64df9358
Prashant V <prashant@prashantv.com>
2023-11-22 18:12:05 -0800

    Add noinline to callers of GetCaller, consistent comments

--- errors.go
+++ errors.go
@@ -8,7 +8,10 @@ import (
 )
 
 // New returns an error that formats as the given text. It's equivalent to
+//
 // [errors.New] followed by [Wrap] to add caller information.
+//
+//go:noinline due to GetCaller (see [Wrap] for details).
 func New(text string) error {
 	return wrap(errors.New(text), pc.GetCaller())
 }
@@ -16,6 +19,8 @@ func New(text string) error {
 // Errorf formats according to a format specifier and returns the string as a
 // value that satisfies error. It's equivalent to [fmt.Errorf] followed by
 // [Wrap] to add caller information.
+//
+//go:noinline due to GetCaller (see [Wrap] for details).
 func Errorf(format string, args ...any) error {
 	return wrap(fmt.Errorf(format, args...), pc.GetCaller())
 }
--- wrap.go
+++ wrap.go
@@ -8,7 +8,7 @@ import (
 // This is intended to be used at all return points in a function.
 // If err is nil, Wrap returns nil.
 //
-//go:noinline since unsafe GetCaller requires a stack and the address of the first arg.
+//go:noinline so caller's PC is saved in the stack frame for asm GetCaller.
 func Wrap(err error) error {
 	if err == nil {
 		return nil
@@ -19,7 +19,7 @@ func Wrap(err error) error {
 
 // Wrap2 is used to [Wrap] the last error return when returning 2 values.
 //
-//go:inline
+//go:noinline due to GetCaller (see [Wrap] for details).
 func Wrap2[T any](t T, err error) (T, error) {
 	if err == nil {
 		return t, nil
@@ -30,7 +30,7 @@ func Wrap2[T any](t T, err error) (T, error) {
 
 // Wrap3 is used to [Wrap] the last error return when returning 3 values.
 //
-//go:noinline for unsafe GetCaller (see [Wrap] for details).
+//go:noinline due to GetCaller (see [Wrap] for details).
 func Wrap3[T1, T2 any](t1 T1, t2 T2, err error) (T1, T2, error) {
 	if err == nil {
 		return t1, t2, nil
@@ -41,7 +41,7 @@ func Wrap3[T1, T2 any](t1 T1, t2 T2, err error) (T1, T2, error) {
 
 // Wrap4 is used to [Wrap] the last error return when returning 4 values.
 //
-//go:noinline for unsafe GetCaller (see [Wrap] for details).
+//go:noinline due to GetCaller (see [Wrap] for details).
 func Wrap4[T1, T2, T3 any](t1 T1, t2 T2, t3 T3, err error) (T1, T2, T3, error) {
 	if err == nil {
 		return t1, t2, t3, nil
@@ -52,7 +52,7 @@ func Wrap4[T1, T2, T3 any](t1 T1, t2 T2, t3 T3, err error) (T1, T2, T3, error) {
 
 // Wrap5 is used to [Wrap] the last error return when returning 5 values.
 //
-//go:noinline for unsafe GetCaller (see [Wrap] for details).
+//go:noinline due to GetCaller (see [Wrap] for details).
 func Wrap5[T1, T2, T3, T4 any](t1 T1, t2 T2, t3 T3, t4 T4, err error) (T1, T2, T3, T4, error) {
 	if err == nil {
 		return t1, t2, t3, t4, nil
@@ -63,7 +63,7 @@ func Wrap5[T1, T2, T3, T4 any](t1 T1, t2 T2, t3 T3, t4 T4, err error) (T1, T2, T
 
 // Wrap6 is used to [Wrap] the last error return when returning 6 values.
 //
-//go:noinline for unsafe GetCaller (see [Wrap] for details).
+//go:noinline due to GetCaller (see [Wrap] for details).
 func Wrap6[T1, T2, T3, T4, T5 any](t1 T1, t2 T2, t3 T3, t4 T4, t5 T5, err error) (T1, T2, T3, T4, T5, error) {
 	if err == nil {
 		return t1, t2, t3, t4, t5, nil
