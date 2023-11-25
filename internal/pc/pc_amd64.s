8c9fe49d8b832649145b8e5fe5cfbb20fb81ccb1
Prashant Varanasi <prashant@prashantv.com>
2023-11-25 12:03:57 -0800

    pc: Remove GetCaller wrapper for asm (#31)
    The `GetCaller` wrapper is only used to call the assembly function but
    to avoid it adding an unnecessary frame, we use `go:inline`. This does
    not work when inlining is disabled, so instead remove the wrapper and
    have the assembly function be exported directly.
    
    Update the test target to verify that the tests pass without inlining
    and optimizations, which fails without this fix.
    
    Fixes #30

--- internal/pc/pc_amd64.s
+++ internal/pc/pc_amd64.s
@@ -2,8 +2,8 @@
 
 #include "textflag.h"
 
-// func getcallerpc() uintptr
-TEXT ·getcallerpc(SB),NOSPLIT|NOFRAME,$0-8
+// func GetCaller() uintptr
+TEXT ·GetCaller(SB),NOSPLIT|NOFRAME,$0-8
 	// BP is the hardware register frame pointer, as used in:
 	// https://github.com/golang/go/blob/go1.21.4/src/runtime/asm_amd64.s#L2091-L2093
 	// The return address sits one word above, hence we evaluate `*(BP+8)`.
