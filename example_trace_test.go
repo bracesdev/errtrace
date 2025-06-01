e398330871387770b37dcbcd2977b273510da958
Abhinav Gupta <mail@abhinavg.net>
2025-06-01 11:09:08 -0700

    fix: local path agnostic output assertions

--- example_trace_test.go
+++ example_trace_test.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"os"
 	"runtime"
 	"strings"
 
@@ -86,24 +85,28 @@ func ExampleUnwrapFrame() {
 func ExampleLogAttr() {
 	// This example demonstrates use of the LogAttr function.
 	// The LogAttr function always uses the "error" key.
-	logger := newExampleLogger()
+	logger, printLogOutput := newExampleLogger()
 
 	if err := f1(); err != nil {
 		logger.Error("f1 failed", errtrace.LogAttr(err))
 	}
 
+	printLogOutput()
+
 	// Output:
-	// {"level":"ERROR","msg":"f1 failed","error":"failed\n\nbraces.dev/errtrace_test.f3\n\t/Users/abg/src/braces.dev/errtrace/example_trace_test.go:24\nbraces.dev/errtrace_test.f2\n\t/Users/abg/src/braces.dev/errtrace/example_trace_test.go:20\nbraces.dev/errtrace_test.f1\n\t/Users/abg/src/braces.dev/errtrace/example_trace_test.go:16\n"}
+	// {"level":"ERROR","msg":"f1 failed","error":"failed\n\nbraces.dev/errtrace_test.f3\n\t/path/to/errtrace/example_trace_test.go:3\nbraces.dev/errtrace_test.f2\n\t/path/to/errtrace/example_trace_test.go:2\nbraces.dev/errtrace_test.f1\n\t/path/to/errtrace/example_trace_test.go:1\n"}
 }
 
 func ExampleLogAttr_noTrace() {
 	// LogAttr reports the original error message
 	// if the error does not have a trace attached to it.
-	logger := newExampleLogger()
+	logger, printLogOutput := newExampleLogger()
 
 	if err := errors.New("no trace"); err != nil {
 		logger.Error("something broke", errtrace.LogAttr(err))
 	}
+
+	printLogOutput()
 
 	// Output:
 	// {"level":"ERROR","msg":"something broke","error":"no trace"}
@@ -113,25 +116,32 @@ func Example_logWithSlog() {
 	// This example demonstrates how to log an errtrace-wrapped error
 	// with the slog package.
 	// Unlike LogAttr, we're able to use any key name here.
-	logger := newExampleLogger()
+	logger, printLogOutput := newExampleLogger()
 
 	if err := f1(); err != nil {
 		logger.Error("f1 failed", "my-error", err)
 	}
 
+	printLogOutput()
+
 	// Output:
-	// {"level":"ERROR","msg":"f1 failed","my-error":"failed\n\nbraces.dev/errtrace_test.f3\n\t/Users/abg/src/braces.dev/errtrace/example_trace_test.go:24\nbraces.dev/errtrace_test.f2\n\t/Users/abg/src/braces.dev/errtrace/example_trace_test.go:20\nbraces.dev/errtrace_test.f1\n\t/Users/abg/src/braces.dev/errtrace/example_trace_test.go:16\n"}
+	// {"level":"ERROR","msg":"f1 failed","my-error":"failed\n\nbraces.dev/errtrace_test.f3\n\t/path/to/errtrace/example_trace_test.go:3\nbraces.dev/errtrace_test.f2\n\t/path/to/errtrace/example_trace_test.go:2\nbraces.dev/errtrace_test.f1\n\t/path/to/errtrace/example_trace_test.go:1\n"}
 }
 
 // newExampleLogger creates a new slog.Logger for use in examples.
-// It omits timestamps from the output to allow for output matching.
-func newExampleLogger() *slog.Logger {
-	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if len(groups) == 0 && a.Key == slog.TimeKey {
-				return slog.Attr{}
-			}
-			return a
-		},
-	}))
+// It omits timestamps from the output to allow for output matching,
+// and cleans paths in trace output to make them environment-agnostic.
+func newExampleLogger() (logger *slog.Logger, printOutput func()) {
+	var buf strings.Builder
+	return slog.New(slog.NewJSONHandler(&buf, &slog.HandlerOptions{
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if len(groups) == 0 && a.Key == slog.TimeKey {
+					return slog.Attr{}
+				}
+				return a
+			},
+		})), func() {
+			fmt.Println(tracetest.MustClean(buf.String()))
+			buf.Reset()
+		}
 }
